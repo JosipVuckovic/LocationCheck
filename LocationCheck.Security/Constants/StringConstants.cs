61c108309a672028a2e16d78614129d58ca51956
JosipVuckovic <josip.vuckovic@gmail.com>
2024-05-20 18:26:32 +0200

    UPD: - added string constants - project reference to data - change of magic strings with string constants (should be resource files) - users are now checked with db call

---
+++ LocationCheck.Security/Constants/StringConstants.cs
@@ -0,0 +1,10 @@
+﻿namespace LocationCheck.Security.Constants;
+
+internal static class StringConstants
+{
+   public const string Authorization = "Authorization";
+   public const string Unauthorised = "Unauthorised";
+   public const string Basic = "Basic";
+   public const string MissingAuthHeader = "Missing Auth Header";
+   public const string InvalidHeader = "Invalid Auth Header";
+}
--- LocationCheck.Security/Handlers/BasicAuthentication.cs
+++ LocationCheck.Security/Handlers/BasicAuthentication.cs
@@ -2,54 +2,70 @@
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Net.Http.Headers;
 using System.Security.Principal;
-using System.Text;
 using System.Text.Encodings.Web;
-using System.Threading.Tasks;
+using LocationCheck.Data;
+using LocationCheck.Security.Constants;
+using Microsoft.EntityFrameworkCore;
 
 namespace LocationCheck.Security.Handlers
 {
     public class BasicAuthenticationHandler : AuthenticationHandler<AuthenticationSchemeOptions>
     {
         private readonly IHttpContextAccessor _httpContextAccessor;
+        private readonly LocationCheckDb _locationCheckDb;
 
         public BasicAuthenticationHandler(IOptionsMonitor<AuthenticationSchemeOptions> options,
                                           ILoggerFactory logger,
                                           UrlEncoder encoder,
                                           ISystemClock clock,
-                                          IHttpContextAccessor httpContextAccessor)
+                                          IHttpContextAccessor httpContextAccessor,
+                                          LocationCheckDb locationCheckDb)
                                           : base(options, logger, encoder, clock)
         {
             _httpContextAccessor = httpContextAccessor;
+            _locationCheckDb = locationCheckDb;
         }
 
         //TODO: move all those strings to constants
         protected override async Task<AuthenticateResult> HandleAuthenticateAsync()
         {
-            if (!Request.Headers.ContainsKey("Authorization"))
+            if (!Request.Headers.ContainsKey(StringConstants.Authorization))
             {
-                return AuthenticateResult.Fail("Missing Auth header");
+                return AuthenticateResult.Fail(StringConstants.MissingAuthHeader);
             }
 
-            if (!AuthenticationHeaderValue.TryParse(Request.Headers["Authorization"], out var authenticationHeader))
+            if (!AuthenticationHeaderValue.TryParse(Request.Headers[StringConstants.Authorization], out var authenticationHeader))
             {
-                return AuthenticateResult.Fail("Invalid header value");
+                return AuthenticateResult.Fail(StringConstants.InvalidHeader);
             }
 
-            if(!string.Equals(authenticationHeader.Scheme, "Basic", StringComparison.InvariantCultureIgnoreCase))
+            if(!string.Equals(authenticationHeader.Scheme, StringConstants.Basic, StringComparison.InvariantCultureIgnoreCase))
             {
-                return AuthenticateResult.Fail("Unauthorised");
+                return AuthenticateResult.Fail(StringConstants.Unauthorised);
             }
             
-            //TODO: Here load user data
+            if (string.IsNullOrEmpty(authenticationHeader.Parameter))
+            {
+                return AuthenticateResult.Fail(StringConstants.Unauthorised);
+            }
+            
+            if (!Guid.TryParse(authenticationHeader.Parameter, out var apiKey))
+            {
+                return AuthenticateResult.Fail(StringConstants.Unauthorised);
+            }
 
-            var identity = new GenericIdentity("Username");
-            var principal = new GenericPrincipal(identity, new[] { "Username" });
-            var ticket = new AuthenticationTicket(principal, new AuthenticationProperties(), "Basic");
+            var user = await _locationCheckDb.ApiUsers.Where(_ => _.ApiKey == apiKey).FirstOrDefaultAsync();
+
+            if (user is null)
+            {
+                return AuthenticateResult.Fail(StringConstants.Unauthorised);
+            }
+            
+            var identity = new GenericIdentity(user.Id.ToString());
+            var principal = new GenericPrincipal(identity, null);
+            var ticket = new AuthenticationTicket(principal, new AuthenticationProperties(), StringConstants.Basic);
 
             return AuthenticateResult.Success(ticket);
         }
