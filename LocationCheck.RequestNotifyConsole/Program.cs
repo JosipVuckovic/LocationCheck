ee86c78d6095f4cee78935fcb3a18931b33b0984
JosipVuckovic <josip.vuckovic@gmail.com>
2024-05-21 14:13:52 +0200

    ADD: - first version of SignlaR console app to show requests made in api, still need to add appsettings

---
+++ LocationCheck.RequestNotifyConsole/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using LocationCheck.RequestNotifyConsole;
+
+var signalRConnection = new SignalRConnection();
+await signalRConnection.Start();
+
+Console.Read();
+
+
---
+++ LocationCheck.RequestNotifyConsole/SignalRHubConnection.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http.Connections;
+
+namespace LocationCheck.RequestNotifyConsole
+{
+    public class SignalRConnection
+    {
+        public async Task Start()
+        {
+            var url = "http://localhost:5100/requestlogs";
+
+            var connection = new HubConnectionBuilder()
+                .WithUrl(url)
+                .WithAutomaticReconnect()
+                .Build();          
+
+            // receive a message from the hub
+            connection.On<string>("broadcastLog", OnReceiveMessage);           
+
+            await connection.StartAsync();
+        }
+
+        private void OnReceiveMessage(string message)
+        {
+            Console.WriteLine($"{message}");
+        }
+
+    }
+}
