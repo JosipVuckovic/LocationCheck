15cb3caef473fc520002b12d936961ab7455a315
JosipVuckovic <josip.vuckovic@gmail.com>
2024-05-24 13:05:19 +0200

    ADD: - added a mapper to map BasicPlace entity to DTO

---
+++ LocationCheck.API/Helpers/Mappers.cs
@@ -0,0 +1,17 @@
+﻿using LocationCheck.API.Models;
+using LocationCheck.Data.Entities;
+
+namespace LocationCheck.API.Helpers;
+
+public static class Mappers
+{
+    public static BasicPlacesDTO MapToDto(this PlaceBasicDataEntity entity)
+    {
+        return new BasicPlacesDTO
+        {
+            ExternalIdentifier = entity.ExternalIdentifier,
+            PlaceName = entity.PlaceName,
+            PlaceTypes = entity.PlaceType.Split('|')
+        };
+    }
+}
